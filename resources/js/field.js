634ca893f28bb077e2220fdb27f696d875bd3a09
kenshikento <tien-nguyen1@hotmail.co.uk>
2022-07-18 10:21:42 +0100

    test

--- resources/js/field.js
+++ resources/js/field.js
@@ -2,8 +2,8 @@ import IndexField from './components/IndexField'
 import DetailField from './components/DetailField'
 import FormField from './components/FormField'
 
-Nova.booting((app, store) => {
-    app.component('index-novafieldcheckboxes', IndexField)
-    app.component('detail-novafieldcheckboxes', DetailField)
-    app.component('form-novafieldcheckboxes', FormField)
+Nova.booting((Vue) => {
+    Vue.component('index-novafieldcheckboxes', IndexField)
+    Vue.component('detail-novafieldcheckboxes', DetailField)
+    Vue.component('form-novafieldcheckboxes', FormField)
 })
